8284a85f4fa4c2078ef5d7a84e597113a909fd0d
Brandon Spicer <brandon.david.spicer@gmail.com>
2022-12-24 20:57:37 -0800

    Add calcSeperation function to boids

--- boids.js
+++ boids.js
@@ -1,14 +1,15 @@
 function boids() {
   const canvas = document.getElementById("myCanvas");
-  canvas.width = 400;
-  canvas.height = 400;
+  canvas.width = 600;
+  canvas.height = 600;
   const ctx = canvas.getContext("2d");
+  const SIZE = 2;
 
   // state
   // each boid has location, velocity
 
   // Number of boids
-  const N = 10000;
+  const N = 1000;
   const W = canvas.width;
   const H = canvas.height;
 
@@ -17,8 +18,8 @@ function boids() {
 
   // initialize
   for (let i = 0; i < N; i++) {
-    const x = Math.floor(Math.random() * W);
-    const y = Math.floor(Math.random() * H);
+    const x = Math.floor((Math.random() * W) / SIZE);
+    const y = Math.floor((Math.random() * H) / SIZE);
     locations.push([x, y]);
 
     const dx = 5 - Math.floor(Math.random() * 10);
@@ -33,15 +34,83 @@ function boids() {
       locations[i][1] += velocities[i][1];
     }
   }
-  // update location with velocity
-  // calculate new velocities
+
+  // distance function
+  function getDistance(vec1, vec2) {
+    let x = vec1[0] - vec2[0];
+    let y = vec1[1] - vec2[1];
+    return Math.sqrt(x * x + y * y);
+  }
+
+  const RADIUS = 5;
+
+  // calculate seperation
+  // Add up all distance vectors in radius
+  // Negate
+  // Add to velocity
+
+  function calcSeperation() {
+    for (let i = 0; i < N; i++) {
+      // sum of distances
+      let sum_x = 0;
+      let sum_y = 0;
+
+      for (let j = 0; j < N; j++) {
+        // if in radius, add to sums
+        let dist = getDistance(locations[i], locations[j]);
+        if (dist <= RADIUS) {
+          sum_x += locations[i][0] - locations[j][0];
+          sum_y += locations[i][1] - locations[j][1];
+        }
+      }
+
+      sum_x = -sum_x / 4;
+      sum_y = -sum_y / 4;
+      velocities[i][0] += sum_x;
+      velocities[i][1] += sum_y;
+    }
+  }
+
+  // calculate alignment
+  // Add up all velocity vectors in radius
+  // Normalize
+  // Add to velocity
+  function calcAlignment() {
+    for (let i = 0; i < N; i++) {
+      // sum of velocites
+      let sum_vx = 0;
+      let sum_vy = 0;
+
+      for (let j = 0; j < N; j++) {
+        // if in radius, add to sums
+        let dist = getDistance(locations[i], locations[j]);
+        if (dist <= RADIUS) {
+          sum_vx += velocities[j][0];
+          sum_vy += velocities[j][1];
+        }
+      }
+
+      sum_x = -sum_x;
+      sum_y = -sum_y;
+      velocities[i][0] += sum_x;
+      velocities[i][1] += sum_y;
+    }
+  }
+
+  // calculate cohesion
+  // Add up all location vectors in radius
+  // Normalize
+  // Add to velocity
+
   // update velocities
+
+  function updateVelocities() {}
 
   // draw
   function draw() {
     ctx.clearRect(0, 0, W, H);
     for (let i = 0; i < N; i++) {
-      ctx.fillRect(locations[i][0], locations[i][1], 1, 1);
+      ctx.fillRect(locations[i][0] * SIZE, locations[i][1] * SIZE, SIZE, SIZE);
     }
   }
 
@@ -49,6 +118,7 @@ function boids() {
   function run() {
     draw();
     updateLocation();
+    calcSeperation();
     setTimeout(run, 10);
   }
 
